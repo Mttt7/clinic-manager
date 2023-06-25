b2e6e4e528c157fc1d097dde708dd4e9cc592dd7
Mttt7 <groobytt@gmail.com>
2023-06-25 11:02:51 +0200

    fix bugs

--- client/clinic-app/src/app/sidebar/sidebar.component.ts
+++ client/clinic-app/src/app/sidebar/sidebar.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  navbarDisabled: boolean = true
+  toggleNavbar() {
+    this.navbarDisabled = !this.navbarDisabled
+  }
 }
